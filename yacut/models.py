292771369469e2bfb8a6e1ead40281af06c75167
Алмаз Миннибаев <almaz200@bk.ru>
2023-09-24 23:14:13 +0300

    add files

--- yacut/models.py
+++ yacut/models.py
@@ -4,4 +4,7 @@ from yacut import db
 
 
 class URLMap(db.Model):
-    ...
+    id = db.Column(db.Integer, primary_key=True)
+    original = db.Column()
+    short = db.Column()
+    timestamp = db.Column(db.String(256))
